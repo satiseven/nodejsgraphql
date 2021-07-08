ec8e0ce845e7aa348a58290e56b359d0050d309d
Riza <satiseven777@gmail.com>
2021-07-08 22:36:20 +0300

    apollo server is installed

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,26 @@
 import { MikroORM } from "@mikro-orm/core";
 import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
-
+import express from "express";
+import { buildSchema } from "type-graphql";
+import { ApolloServer } from "apollo-server-express";
+import { HelloResolver } from "./resolvers/hello";
 const main = async () => {
+  const app = express();
   const orm = await MikroORM.init(microConfig);
-  const post = orm.em.create(Post, { title: "Selam " });
-  await orm.em.persistAndFlush(post);
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+  apolloServer.applyMiddleware({ app });
+  app.get("/", (_, res) => {
+    res.send("sadfasdf");
+  });
+  app.listen(4000, () => {
+    console.log(`Server is running on port http://localhost:4000`);
+  });
 };
 
 main();
