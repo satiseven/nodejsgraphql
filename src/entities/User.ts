c88bc5b352ba7bddec47a3b4f3fd9ae0ad7d9e1b
Riza <satiseven777@gmail.com>
2021-07-11 16:29:41 +0300

    user migration

---
+++ src/entities/User.ts
@@ -0,0 +1,26 @@
+import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, ID } from "type-graphql";
+
+@Entity()
+export class User {
+  @Field(() => ID)
+  @PrimaryKey({ name: "id" })
+  id!: number;
+  @Field(() => String)
+  @Property({ name: "name" })
+  name: string;
+  @Field(() => String)
+  @Property()
+  email: string;
+  @Field(() => String)
+  @Property({ name: "username", unique: true })
+  username!: string;
+  @Field(() => String)
+  @Property({ name: "password", type: "text" })
+  password: string;
+  @Field(() => String)
+  @Property({ type: "date" })
+  createdAt = new Date();
+  @Property({ type: "date", onUpdate: () => new Date() })
+  updatedAt = new Date();
+}
