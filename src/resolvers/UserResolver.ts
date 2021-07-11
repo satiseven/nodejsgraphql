b8467747a6cb10ba2608f09b72aca40824af6b61
Riza <satiseven777@gmail.com>
2021-07-11 17:58:45 +0300

    Field Error

--- src/resolvers/UserResolver.ts
+++ src/resolvers/UserResolver.ts
@@ -1,6 +1,14 @@
 import { User } from "../entities/User";
 import argon2 from "argon2";
-import { Arg, Ctx, Field, InputType, Mutation, Resolver } from "type-graphql";
+import {
+  Arg,
+  Ctx,
+  Field,
+  InputType,
+  Mutation,
+  ObjectType,
+  Resolver,
+} from "type-graphql";
 import { MyContext } from "../types/MyContext";
 @InputType()
 class UserInputs {
@@ -13,8 +21,44 @@ class UserInputs {
   @Field()
   password: string;
 }
+@InputType()
+class LoginInputs {
+  @Field()
+  username: string;
+  @Field()
+  password: string;
+}
+@ObjectType()
+class FiledError {
+  @Field()
+  field: string;
+  message: string;
+}
+@ObjectType()
+class UserResponse {
+  @Field(() => [FiledError], { nullable: true })
+  errors?: FiledError[];
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
 @Resolver()
 export class UserResolver {
+  @Mutation(() => Boolean)
+  async Login(
+    @Arg("options", () => LoginInputs) options: LoginInputs,
+    @Ctx() { em }: MyContext
+  ) {
+    try {
+      const user = await em.findOneOrFail(User, {
+        username: options.username.toLowerCase(),
+      });
+      const hashedPassword = await argon2.hash(options.password);
+      argon2.verify;
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
   @Mutation(() => User)
   async register(
     @Arg("options", () => UserInputs) options: UserInputs,
